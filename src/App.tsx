bbeffa0d21287143e6d199f77572fd3f8dab4309
Artsem Zhurauski <aazhuravskiy@sberbank.ru>
2020-10-15 20:14:42 +0300

    added react routed pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AdminPage from "./pages/AdminPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className="App">TyZa</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route component={HomePage} exact path="/"></Route>
+          <Route path="/admin" exact component={AdminPage}>
+            <div>222</div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ui/colors.tsx
@@ -0,0 +1,4 @@
+export const colors: { [key: string]: string } = {
+  silk: "C1B3A8",
+  seashellPeach: "#FFF7F1",
+};
